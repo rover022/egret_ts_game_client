e95bbe1efb1661d4e87f101a9c6585db9760bc1b
rover022 <rover022@163.com>
2018-04-20 17:41:19 +0800

    22

--- src/view/ViewLogin.ts
+++ src/view/ViewLogin.ts
@@ -1,5 +1,9 @@
 ///<reference path="../manager/GameManager.ts"/>
 module ui {
+
+
+    import VerticalAlign = egret.VerticalAlign;
+
     export class ViewLogin extends eui.Component {
         private startBtn: eui.Button;
 
@@ -34,8 +38,9 @@ module ui {
         funCanKuBtn: eui.Button;
         funSuihuanBtn: eui.Button;
         room0Btn: eui.Button;
-        room1Btn: eui.Button
-        room2Btn: eui.Button
+        room1Btn: eui.Button;
+        room2Btn: eui.Button;
+        myScroller: eui.Scroller;
 
         public constructor() {
             super();
@@ -57,11 +62,30 @@ module ui {
             this.funSuihuanBtn.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
 
             }, this);
+            //建立房间图片
+            // this.myScroller
+            //let list: eui.List = new eui.List();
+            //list.dataProvider = new eui.ArrayCollection([1, 2, 3]);
+            //this.myScroller.viewport = list;
+            // var hLayout: eui.HorizontalLayout = new eui.HorizontalLayout();
+            // hLayout.gap = 0;
+            //
+            // hallGroup.layout =
+
             //牛牛房间
             this.room0Btn.addEventListener(egret.TouchEvent.TOUCH_TAP, () => {
                 let room: ViewGameRoom = new ViewGameRoom();
                 GameManager.getInstance().popNewGameRoom(room);
-            }, this)
+            }, this);
+            console.log(this.myScroller.viewport);
+            let mygroup: eui.Group = this.myScroller.viewport as eui.Group;
+            let layout = new eui.HorizontalLayout();
+            layout.gap = 20;
+            layout.verticalAlign = VerticalAlign.MIDDLE;
+            mygroup.layout = new eui.HorizontalLayout();
+            for (let i = 0; i < 20; i++) {
+                mygroup.addChild(new RoomImageCard());
+            }
         }
 
         private onbeVipHandle() {
@@ -77,5 +101,42 @@ module ui {
         }
     }
 
+    class RoomCard extends eui.Button {
+        constructor() {
+            super();
+            this.icon = RES.getRes("")
+        }
 
+    }
+
+    class RoomImageCard extends eui.Image {
+        constructor() {
+            super();
+            this.touchEnabled = true;
+            this.source = RES.getRes("ting1_n_png");
+            this.addEventListener(egret.TouchEvent.TOUCH_BEGIN, this.onTouchBeginHandle, this);
+            this.addEventListener(egret.TouchEvent.TOUCH_CANCEL, this.onTouchEndHandle, this);
+            this.addEventListener(egret.TouchEvent.TOUCH_END, this.onTouchEndHandle, this);
+            this.addEventListener(egret.TouchEvent.TOUCH_RELEASE_OUTSIDE, this.onTouchEndHandle, this);
+
+        }
+
+        private onclickHandle(e: TouchEvent) {
+            console.log("onclickHandle");
+        }
+
+        private onTouchBeginHandle() {
+            // this.scaleX = this.scaleY = 0.95;
+            // this.rotation = 45;
+            // this.y = 10;
+            this.alpha = 0.8;
+        }
+
+        private onTouchEndHandle() {
+            this.alpha = 1;
+            this.scaleX = this.scaleY = 1;
+            // this.rotation = 0;
+            // this.y = 0;
+        }
+    }
 }
